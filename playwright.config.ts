6c216ff19a81faad51dde3b4d0c5fbf6f9c03f1e
Joe Heck <heckj@mac.com>
2025-11-24 14:57:24 -0800

    shifting around a bit

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,25 +1,25 @@
 import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
-    testDir: './tests',
+    testDir: './playwright-tests',
     fullyParallel: true,
     forbidOnly: !!process.env.CI,
     retries: process.env.CI ? 2 : 0,
     workers: process.env.CI ? 1 : undefined,
     reporter: 'html',
     use: {
-        baseURL: 'http://localhost:4000/documentation/exampledocs',
+        baseURL: 'http://localhost:4000/documentation',
         trace: 'on-first-retry',
     },
     projects: [
         {
-            name: 'chromium',
-            use: { ...devices['Desktop Chrome'] },
+            name: 'webkit',
+            use: { browserName: 'webkit' },
         },
     ],
     webServer: {
         command: './scripts/build-and-serve.sh',
-        url: 'http://localhost:4000/documentation/exampledocs',
+        url: 'http://localhost:4000/documentation',
         reuseExistingServer: true,
         stdout: 'pipe',
         stderr: 'pipe',
