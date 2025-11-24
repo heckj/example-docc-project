2d8349885c49a89e5b01d2f7adc6e94b84b8f44e
Joe Heck <heckj@mac.com>
2025-11-24 13:59:18 -0800

    verify navbar sees both catalogs

--- tests/docc.spec.ts
+++ tests/docc.spec.ts
@@ -1,3 +1,4 @@
+// @ts-check
 import { test, expect } from '@playwright/test';
 
 test('Base directory has combined content', async ({ page }) => {
@@ -5,4 +6,11 @@ test('Base directory has combined content', async ({ page }) => {
 
     // Expect a title "to contain" a substring.
     await expect(page).toHaveTitle(/example-docc-project/);
+
+    // Expect the sidebar navigation to be available
+    const sidebar = page.locator('.navigator-card');
+    await expect(sidebar).toBeVisible();
+    await expect(sidebar).toHaveText(/ExampleDocs/);
+    await expect(sidebar).toHaveText(/SampleLibrary/);
 });
+    
